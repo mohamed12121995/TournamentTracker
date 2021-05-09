a253e59719028a178ed52e0ca632270ed4547592
mohamed.nuur43@outlook.com <mohamed.nuur43@outlook.com>
2021-05-09 14:50:48 +0100

    Changes 09/05

---
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        /// <summary>
+        /// List of IDataConnection allows to connect to multiple data sources
+        /// </summary>
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+    
+        public static void InitializeConnections(bool database, bool textFiles)
+        {// wire up the classes
+            if(database)
+            {
+                // TODO - Set up the SQL connector properly
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+
+            if (textFiles)
+            {
+                // TODO - Create the text connection
+            }
+
+        }
+    }
+}
